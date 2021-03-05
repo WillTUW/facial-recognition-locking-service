0ecfda1fd9836d46c7658ce7b55fd731418aa93e
Will Thomas <44074503+WillTUW@users.noreply.github.com>
2021-03-04 18:50:11 -0800

    finalNotes

--- deviceCode/lockingDevice/lock.py
+++ deviceCode/lockingDevice/lock.py
@@ -6,9 +6,11 @@ import time
 
 pinName = 18
 subTopic = 'Unlock'
+
+# Set up GPIO for the lock
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setuprelay(GPIO.OUT)
+GPIO.setup(pinName, GPIO.OUT)
 GPIO.output(pinName, 0)
 
 
@@ -31,11 +33,21 @@ def faceWasKey(client, arg, arg2):
 
     print('Device Locked')
 
-
+# Main function to be run on the lock device.
+# It creates an MQTT client and subs to a topic
+# that receives messages when a key is found. 
 def runLock():
+
+    # Make the client with out make client file
     client = makeClient()
 
+    # Subscribe to the topic, go to faceWasKey method if
+    # a message is received
     client.subscribe(subTopic, 1, faceWasKey)
+
+    # Loop forever
     while True:
         time.sleep(1)
 
+
+runLock()
