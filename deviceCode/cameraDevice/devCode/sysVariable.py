6efe345caaacc2f19eb543815548dc19e286106e
Will Thomas <44074503+WillTUW@users.noreply.github.com>
2021-03-05 15:34:55 -0800

    Final Commenting

--- deviceCode/cameraDevice/devCode/sysVariable.py
+++ deviceCode/cameraDevice/devCode/sysVariable.py
@@ -1,16 +1,43 @@
-""" System Varaibes to be used with non test
+""" System Varaibes
 
-
+For testing, assign the variables according to the comments given
 """
+
+# Constant camera monitoring path. 
+# Reassign to pre determined face for testing. As well as comment
+# Out the takePicture function. 
 IMGPATH = ''
+
+# Assign to the HaarCascade Front facing model
+# Can be found here: 
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 FACE_DETECTION_MODULE = ''
+
+# Used to store new user faces temporarly. Lets us encode it
+# Reassign this to a pre determined picture for testing
 NEWFACE = ''
+
+# Root-CA that should look like [yourpath]root-CA.crt
 ROOTCAPATH = ''
+
+# Private key in the form of [deviceThing].private.key
 PRIVATEKEYPATH = ''
+
+# Certification path in the form of [deviceName].cert.pem
 CERTIFICATEPATH = ''
+
+# Host information derived from the IoT thing
 HOST = ''
+
+# this devices name (thing name)
 DEVICENAME = ''
+
+# Collection ID that is stored locally. 
 FACE_COLLECTION_ID = ''
+
+# Unused variable for collection creation
 COLLECTION_CREATED = ''
-PATH_TO_USERS = ' .json'
+
+# Location that the JSON file for users will be written too
+PATH_TO_USERS = ''
 
---
+++ deviceCode/lockingDevice/sysVariable.py
@@ -0,0 +1,20 @@
+""" System Varaibes
+
+"""
+
+
+# Root-CA that should look like [yourpath]root-CA.crt
+ROOTCAPATH = ''
+
+# Private key in the form of [deviceThing].private.key
+PRIVATEKEYPATH = ''
+
+# Certification path in the form of [deviceName].cert.pem
+CERTIFICATEPATH = ''
+
+# Host information derived from the IoT thing
+HOST = ''
+
+# this devices name (thing name)
+DEVICENAME = ''
+
